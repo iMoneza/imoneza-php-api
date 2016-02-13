eb10f0acdcf5554e2de8bae93cae95c014f9c8a8
Aaron Saray <me@aaronsaray.com>
2016-02-12 19:02:16 -0600

    added some unit tests.

---
+++ tests/iMoneza/Options/Access/ResourceTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * test
+ *
+ * @author Aaron Saray
+ */
+
+namespace iMoneza\Tests\Options\Access;
+
+use iMoneza\Options\Access\Resource;
+
+/**
+ * Class ResourceTest
+ * @package iMoneza\Tests\Options\Access
+ */
+class ResourceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetAccessKey()
+    {
+        $options = new Resource();
+        $this->assertInstanceOf('iMoneza\Options\Access\Resource', $options->setAccessKey('access-key'));
+        $this->assertAttributeEquals('access-key', 'accessKey', $options);
+    }
+
+    public function testSetResourceKey()
+    {
+        $options = new Resource();
+        $this->assertInstanceOf('iMoneza\Options\Access\Resource', $options->setResourceKey('resource-key'));
+        $this->assertAttributeEquals('resource-key', 'resourceKey', $options);
+    }
+
+    public function testSetResourceURL()
+    {
+        $options = new Resource();
+        $this->assertInstanceOf('iMoneza\Options\Access\Resource', $options->setResourceURL('resource-url'));
+        $this->assertAttributeEquals('resource-url', 'resourceURL', $options);
+    }
+
+    public function testSetUserToken()
+    {
+        $options = new Resource();
+        $this->assertInstanceOf('iMoneza\Options\Access\Resource', $options->setUserToken('user-token'));
+        $this->assertAttributeEquals('user-token', 'userToken', $options);
+    }
+
+    public function testSetIpAddress()
+    {
+        $options = new Resource();
+        $this->assertInstanceOf('iMoneza\Options\Access\Resource', $options->setIpAddress('123.123.123.123'));
+        $this->assertAttributeEquals('123.123.123.123', 'ipAddress', $options);
+    }
+
+    public function testGetRequestType()
+    {
+        $options = new Resource();
+        $this->assertEquals('get', $options->getRequestType());
+    }
+
+    public function testGetEndPoint()
+    {
+        $options = new Resource();
+        $options->setAccessKey('access-key')->setResourceKey('resource-key');
+        $this->assertEquals('/api/Resource/access-key/resource-key', $options->getEndPoint());
+    }
+}
---
+++ tests/iMoneza/Options/OptionsAbstractTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Test the populated method
+ *
+ * @author Aaron Saray
+ */
+
+namespace iMoneza\Tests\Options;
+
+use iMoneza\Options\OptionsAbstract;
+
+/**
+ * Class OptionsAbstractTest
+ * @package iMoneza\Tests\Options
+ */
+class OptionsAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetPopulatedEmptyArray()
+    {
+        $stub = $this->getMockForAbstractClass('iMoneza\Options\OptionsAbstract');
+        $this->assertEmpty($stub->getPopulated());
+    }
+
+    public function testGetPopulatedArrayWithNoElementsIsEmpty()
+    {
+        $options = new TestOptions();
+        $this->assertEmpty($options->getPopulated());
+    }
+
+    public function testGetPopulatedArrayWithBothElementsReturnsOnlyProtected()
+    {
+        $options = new TestOptions();
+        $options->setProtectedItem('protected-item');
+        $options->setPrivateItem('private-item');
+        $this->assertEquals(['protectedItem' => 'protected-item'], $options->getPopulated());
+    }
+}
+
+
+class TestOptions extends OptionsAbstract
+{
+    protected $protectedItem;
+
+    private $privateItem;
+
+    public function setProtectedItem($item)
+    {
+        $this->protectedItem = $item;
+    }
+
+    public function setPrivateItem($item)
+    {
+        $this->privateItem = $item;
+    }
+
+    public function getEndPoint()
+    {}
+
+    public function getRequestType()
+    {}
+}
