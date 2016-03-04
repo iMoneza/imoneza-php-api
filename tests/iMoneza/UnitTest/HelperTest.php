39bc9f6c3011fb4969ddb5a094ad26f22e3a7bf9
Aaron Saray <me@aaronsaray.com>
2016-03-04 14:32:19 -0600

    Updating unit tests.

---
+++ tests/iMoneza/UnitTest/HelperTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Unit Test
+ *
+ * @author Aaron Saray
+ */
+
+namespace iMoneza\UnitTest;
+
+use iMoneza\Helper;
+
+/**
+ * Class HelperTest
+ * @package iMoneza\UnitTest
+ */
+class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRetrieveIP()
+    {
+        $this->assertEmpty(Helper::getCurrentIP());
+
+        putenv('REMOTE_ADDR=8.8.8.8');
+        $this->assertEquals('8.8.8.8', Helper::getCurrentIP());
+
+        putenv('REMOTE_ADDR=""');
+        putenv('HTTP_CLIENT_IP=8.8.4.4, 8.8.8.8');
+        $this->assertEquals('8.8.4.4', Helper::getCurrentIP());
+    }
+}
